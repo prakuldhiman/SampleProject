28483c690f021b6902b223550de92ed3cd8ae9be
prakuldhiman <dhiman404prakul@gmail.com>
2018-05-24 19:56:13 +0530

    First Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { AppRoutingModule } from './routing.module';
+import { MaterialModule } from './material.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    NgbModule.forRoot(),
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+// routes
+const routes: Routes = [
+    // {
+    //     path: 'user',
+    //     loadChildren: 'app/modules/user-module/user.module#UserModule'
+    // },
+
+    { path: '**', redirectTo: '/user' }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
